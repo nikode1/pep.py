4183c60ff544fd0832b72e55b04ba9d00bf466af
Hazuki Onohara <neko.aoba.suzukaze@gmail.com>
2020-03-30 08:45:16 +0300

    Disabled Delta Clients

--- handlers/apiGetTheFuckOuttaHere.py
+++ handlers/apiGetTheFuckOuttaHere.py
@@ -1,12 +1,13 @@
-import tornado.gen
+import json
+
 import tornado.web
+import tornado.gen
 
 from common.web import requestsManager
-
 
 class handler(requestsManager.asyncRequestHandler):
 	@tornado.web.asynchronous
 	@tornado.gen.engine
 	def asyncGet(self):
-		#self.set_status(404)
-		self.write("")
+		self.set_status(200)
+		self.write('{"code": 200, "clients": []}')
