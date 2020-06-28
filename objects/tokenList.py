64b11e6fb01251ee4cc4654386910f7309dd94a3
Hazuki Onohara <neko.aoba.suzukaze@gmail.com>
2020-06-28 09:03:57 +0300

    THANK YOU

--- objects/tokenList.py
+++ objects/tokenList.py
@@ -190,10 +190,12 @@ class tokenList:
 			timeoutLimit = int(time.time()) - 100
 			for key, value in self.tokens.items():
 				# Check timeout (fokabot is ignored)
-				if value.pingTime < timeoutLimit and aobaHelper.getAlwaysOnline(value.userID) == True and not value.irc and not value.tournament:
-					# That user has timed out, add to disconnected tokens
-					# We can't delete it while iterating or items() throws an error
-					timedOutTokens.append(key)
+				#print("UserID {} Always Online: {}".format(value.userID, aobaHelper.getAlwaysOnline(value.userID)))
+				if aobaHelper.getAlwaysOnline(value.userID) == False:
+					if value.pingTime < timeoutLimit and not value.irc and not value.tournament:
+						# That user has timed out, add to disconnected tokens
+						# We can't delete it while iterating or items() throws an error
+						timedOutTokens.append(key)
 
 			# Delete timed out users from self.tokens
 			# i is token string (dictionary key)
