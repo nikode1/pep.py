5e1a93f6adc3ca087f7b1dff26cb68924310d254
Pure-Peace <940857703@qq.com>
2020-08-15 01:36:19 +0800

    tab

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ UNIX = True if os.name == "posix" else False
 
 cythonExt = []
 for root, dirs, files in os.walk(os.getcwd()):
-    for file in files:
-        if file.endswith(".pyx") and ".pyenv" not in root:    # im sorry
-            filePath = os.path.relpath(os.path.join(root, file))
-            cythonExt.append(Extension(filePath.replace("\\" if not UNIX else "/", ".")[:-4], [filePath]))
+	for file in files:
+		if file.endswith(".pyx") and ".pyenv" not in root:    # im sorry
+			filePath = os.path.relpath(os.path.join(root, file))
+			cythonExt.append(Extension(filePath.replace("\\" if not UNIX else "/", ".")[:-4], [filePath]))
 
 setup(
-    name = "pep.pyx modules",
-    ext_modules = cythonize(cythonExt, nthreads = 0 if not UNIX else 4),
+	name = "pep.pyx modules",
+	ext_modules = cythonize(cythonExt, nthreads = 0 if not UNIX else 4),
 )
